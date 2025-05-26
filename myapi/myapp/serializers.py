be47da1026359af1739d99c887a0c7296f7c705d
HelioRivera <sorencen@gmail.com>
2025-05-26 07:02:18 -0400

    add validations

--- myapi/myapp/serializers.py
+++ myapi/myapp/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    resumen = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+        extra_kwargs = {
+            "description": {"required": False} # Hace que description sea opcional
+        }
+    
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number.")
+        return value
+    
+    def get_resumen(self, obj):
+         return f"producto: {obj.name}, precio: {obj.price}"
